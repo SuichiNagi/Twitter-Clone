6a828bf64ea774831c5976e614a6e0842c166e21
SuichiNagi <aldreiglennkim@gmail.com>
2024-08-21 19:58:41 +0800

    Initial Commit

---
+++ Twitter-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter-Clone
+//
+//  Created by Aldrei Glenn Nuqui on 8/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
