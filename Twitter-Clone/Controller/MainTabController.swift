02052eca10e95c6444d830d1675a8f4fc39018a3
SuichiNagi <aldreiglennkim@gmail.com>
2024-08-21 22:18:05 +0800

    Setting up sticky action button

--- Twitter-Clone/Controller/MainTabController.swift
+++ Twitter-Clone/Controller/MainTabController.swift
@@ -6,16 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainTabController: UITabBarController {
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configControllers()
+        setUI()
     }
     
-    func configControllers() {
+    // MARK: - Helpers
+    
+    private func configControllers() {
         tabBar.backgroundColor = .systemGray6
         
         let nav1 = templateNavigationController(rootViewController: feedController)
@@ -26,33 +32,53 @@ class MainTabController: UITabBarController {
         viewControllers = [nav1, nav2, nav3, nav4]
     }
     
-    func templateNavigationController(rootViewController: UIViewController) -> UINavigationController {
+    private func templateNavigationController(rootViewController: UIViewController) -> UINavigationController {
         let nav = UINavigationController(rootViewController: rootViewController)
         nav.navigationBar.barTintColor = .white
         return nav
     }
+    
+    private func setUI() {
+        view.addSubview(actionButton)
+        
+        actionButton.snp.makeConstraints { make in
+            make.height.width.equalTo(56)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-64)
+            make.right.equalToSuperview().offset(-16)
+        }
+    }
 
-    lazy var feedController: FeedController = {
+    private lazy var feedController: FeedController = {
         let feed = FeedController()
         feed.tabBarItem.image = UIImage(named: "home_unselected")
         return feed
     }()
     
-    lazy var exploreController: ExploreController = {
+    private lazy var exploreController: ExploreController = {
         let explore = ExploreController()
         explore.tabBarItem.image = UIImage(named: "search_unselected")
         return explore
     }()
     
-    lazy var notifController: NotificationsController = {
+    private lazy var notifController: NotificationsController = {
         let notif = NotificationsController()
         notif.tabBarItem.image = UIImage(named: "like_unselected")
         return notif
     }()
     
-    lazy var convoController: ConversationsController = {
+    private lazy var convoController: ConversationsController = {
         let convo = ConversationsController()
         convo.tabBarItem.image = UIImage(named: "ic_mail_outline_white_2x-1")
         return convo
+    }()
+    
+    private lazy var actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 56 / 2
+        return button
     }()
 }
